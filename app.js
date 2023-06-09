c0478ad02e99bd39c8c0ffee1e2070e89d657b0d
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2023-06-09 14:28:55 +0530

    First version of todolist

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+const app = express();
+const port = 3000;
+
+const items =["Buy Food","Cook Food","Eat Food"];
+const workItems =[];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
+app.get("/", function (req, res) {
+  let day = date.getDate(); // OR  date.getDay()
+  res.render("List", {
+    listTitle: day,
+    newListItems: items
+  });
+});
+
+app.post("/",function(req,res) {
+   let item = req.body.newItem;
+   if (req.body.list==="Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+   } else {
+   items.push(item);
+   res.redirect("/");
+   }
+});
+
+app.get("/work",function(req,res) {
+  res.render("list",{listTitle:"Work List",newListItems:workItems});
+});
+
+app.post("/",function(req,res) {
+ let item = req.body.newItem;
+ workItems.push(item);
+ res.redirect("/work");
+});
+
+
+app.get("/contact-me",function(req,res) {
+  res.render("contact-me");
+});
+
+
+
+app.listen(port, function () {
+  console.log("Server in running on port " + port + ".");
+});
+
---
+++ date.js
@@ -0,0 +1,21 @@
+
+exports.getDate = function () {
+    const options = {
+        weekday : "long",
+        year : "numeric",
+        month : "long",
+        day : "numeric"
+    };
+    const today = new Date();
+
+  return today.toLocaleDateString("en-US",options);
+}
+
+exports.getDay = function () {
+    const options = {
+        weekday : "long"
+    };
+    const today = new Date();
+
+  return today.toLocaleDateString("en-US",options);
+}
